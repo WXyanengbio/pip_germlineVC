b4eeecc6431f499bc95ab33c406a54b753419bb6
WXyanengbio <wangxian@yanengbio.com>
2018-07-30 08:58:45 +0800

    加上进程控制参数
    防止使用内存溢出

--- pipelines/benchmark/hap_benchmark.py
+++ pipelines/benchmark/hap_benchmark.py
@@ -1,19 +1,14 @@
 from __future__ import barry_as_FLUFL
 
-__all__  =  ['benchmark_dir','truth_vcf', 'filter_vcf', 'confident_region_bed', 'benchmarking_dir', 'total_ref_fa_file', 'exon_interval', 'logger_benchmark_process', 'logger_benchmark_errors']
+__all__  =  ['benchmark_dir','truth_vcf', 'filter_vcf', 'confident_region_bed', 'benchmarking_dir', 'total_ref_fa_file', 'exon_interval', 'num_threads','logger_benchmark_process', 'logger_benchmark_errors']
 __version__  =  '1.0'
 __author__  =  'Wang Xian'
 
 import os
-import re
 import sys
-import time
-import gzip
-import itertools
 
 
-
-def hap_py(benchmark_dir,truth_vcf, filter_vcf, confident_region_bed, benchmarking_dir, total_ref_fa_file, exon_interval, logger_benchmark_process, logger_benchmark_errors):
+def hap_py(benchmark_dir,truth_vcf, filter_vcf, confident_region_bed, benchmarking_dir, total_ref_fa_file, exon_interval, num_threads, logger_benchmark_process, logger_benchmark_errors):
     exon_bed = benchmarking_dir + '/' + os.path.basename(exon_interval).rstrip(".list") + ".bed"
     if not os.path.isfile(exon_bed):
         logger_benchmark_errors.error("%s does not exist!\n", exon_bed)
@@ -26,7 +21,7 @@ def hap_py(benchmark_dir,truth_vcf, filter_vcf, confident_region_bed, benchmarki
         print(filter_vcf + ' does not exist!')
     else:
         benchmarout = benchmarking_dir + '/' + os.path.basename(filter_vcf).rstrip(".vcf")
-        cmd2 = '{0} {1} {2} -f {3} -r {4} -T {5} -o {6}'.format(benchmark_dir, truth_vcf, filter_vcf,confident_region_bed,total_ref_fa_file ,exon_bed, benchmarout)
+        cmd2 = '{0} {1} {2} -f {3} -r {4} -T {5} -o {6} --threads {7}'.format(benchmark_dir, truth_vcf, filter_vcf,confident_region_bed,total_ref_fa_file ,exon_bed, benchmarout,num_threads)
         logger_benchmark_process.info('hap.py benchmark.')
         os.system(cmd2)
     
