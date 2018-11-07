cfec0cbdd20a79a7526fd601bed93eb601888f4c
WXyanengbio <wangxian@yanengbio.com>
2018-11-07 15:01:37 +0800

    remove the seq with low base quality UMI
    median(base quality of UMI) <Q20,  过滤

--- pipelines/trim/trim_reads_v1.py
+++ pipelines/trim/trim_reads_v1.py
@@ -14,6 +14,8 @@ import gzip
 import itertools
 import shlex
 import subprocess
+import numpy
+
 sys.path.append("..")
 from pipelines.log.log_v1 import store_trim_logs
 
@@ -59,7 +61,7 @@ def read_fq(file_name, logger_trim_process, logger_trim_errors):
 def trim_read1(r1, common_seq2, mt_barcode):
     l1 = len(r1[1].strip())
     r1_end3 = r1[1].strip()[(l1 - 23):]
-    #trim_seq = common_seq2 + mt_barcode
+    # trim_seq = common_seq2 + mt_barcode
     trim_seq = reverse_complement(common_seq2 + mt_barcode)
     for i in range(23):
         if r1_end3[i:] == trim_seq[0:(23 - i)]:
@@ -87,7 +89,7 @@ def trim_read_pairs(read1, read2, trimmed1, trimmed2, min_read_len, common_seq1,
     num_error_reads2 = 0
     fout1 = open(trimmed1, 'w')
     fout2 = open(trimmed2, 'w')
-    fout_umi = open(trimmed2 + '.umi.fq', 'w')
+    # fout_umi = open(trimmed2 + '.umi.fq', 'w')
     for r1, r2 in zip(read_fq(read1, logger_trim_process, logger_trim_errors),
                       read_fq(read2, logger_trim_process, logger_trim_errors)):
         num_total_reads += 1
@@ -105,27 +107,37 @@ def trim_read_pairs(read1, read2, trimmed1, trimmed2, min_read_len, common_seq1,
             else:
                 umi = r2[1][(start_common - 12):start_common]
                 qua = r2[3][(start_common - 12):start_common]
-                r2[1] = r2[1][(start_common + 11):]
-                r2[3] = r2[3][(start_common + 11):]
-                pos_trim_r1, r1 = trim_read1(r1, common_seq2, umi)
-                pos_trim_r2, r2 = trim_read2(r2, common_seq1)
-                if pos_trim_r1 < min_read_len or pos_trim_r2 < min_read_len:
-                    num_short_reads += 1
+                # delete umi with low base quality 
+                quanum = list(map(ord, qua))
+                quanum = [i - 33 for i in quanum]
+                # if the median base quality is bigger than Q20 
+                if numpy.median(quanum) < 20 :
+                    num_error_reads2 += 1
                     store_trim_logs('null', logger_trim_errors,
-                                    "Short read pair: \n\t" + '\t'.join(r1) + '\n\t' + '\t'.join(r2) + '\n')
+                                "Error barcode/common seqs:" + str(start_common)
+                                + "\n\t" + '\t'.join(r1) + '\n\t' + '\t'.join(r2) + '\n')
                 else:
-                    h1 = r1[0].split(' ')[0] + '_' + umi + ' ' + r1[0].split(' ')[1]
-                    h2 = r2[0].split(' ')[0] + '_' + umi + ' ' + r2[0].split(' ')[1]
-                    fout1.write(h1 + r1[1] + r1[2] + r1[3])
-                    fout2.write(h2 + r2[1] + r2[2] + r2[3])
+                    r2[1] = r2[1][(start_common + 11):]
+                    r2[3] = r2[3][(start_common + 11):]
+                    pos_trim_r1, r1 = trim_read1(r1, common_seq2, umi)
+                    pos_trim_r2, r2 = trim_read2(r2, common_seq1)
+                    if pos_trim_r1 < min_read_len or pos_trim_r2 < min_read_len:
+                        num_short_reads += 1
+                        store_trim_logs('null', logger_trim_errors,
+                                    "Short read pair: \n\t" + '\t'.join(r1) + '\n\t' + '\t'.join(r2) + '\n')
+                    else:
+                        h1 = r1[0].split(' ')[0] + '_' + umi + ' ' + r1[0].split(' ')[1]
+                        h2 = r2[0].split(' ')[0] + '_' + umi + ' ' + r2[0].split(' ')[1]
+                        fout1.write(h1 + r1[1] + r1[2] + r1[3])
+                        fout2.write(h2 + r2[1] + r2[2] + r2[3])
                     #quanum = list(map(ord, qua))
                     #quanum = [i - 33 for i in quanum]
                     #quanumstr = '\t'.join(list(map(str, quanum)))
                     #fout_umi.write(''.join([r1[0].split(' ')[0], '_', umi, '\t', umi, '\t', quanumstr, '\n']))
-                    fout_umi.write(h2 + umi + '\n' +  '+\n' + qua + '\n')
+                    #fout_umi.write(h2 + umi + '\n' +  '+\n' + qua + '\n')
     fout1.close()
     fout2.close()
-    fout_umi.close()
+    # fout_umi.close()
     stats_out = open(stats_file, 'w')
     stats_out.write('Total number of reads == ' + str(num_total_reads) + '\n')
     stats_out.write('Number of short reads (either read_length <{0}bp) == {1}\n'.format(
@@ -133,6 +145,7 @@ def trim_read_pairs(read1, read2, trimmed1, trimmed2, min_read_len, common_seq1,
     stats_out.write('Number of unproper read pairs (containing incorrect headers) == ' + str(num_error_reads1) + '\n')
     stats_out.write('Number of read pairs without correct common sequences/MTs == ' + str(num_error_reads2) + '\n')
     stats_out.write('The time of trimming is %s minutes.' % str((time.time() - time_start) / 60))
+    stats_out.close()
 
 
 def trim_read_pairs_by_trimmomatic(trimmomatic_dir,
